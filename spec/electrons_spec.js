ac6cff343bd8d6e04e4d3443ac8905c0732b518b
Yemi Akinrinlola <if25b072@technikum-wien.at>
2026-05-23 19:05:23 +0200

    Test 1 und 2 funktionieren nicht

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -1,11 +1,11 @@
 describe("Electrons around the cores", function () {
- /* it("returns 6 for [1, 2, 3, 4, 5]", function () {
+  it("returns 6 for [1, 2, 3, 4, 5]", function () {
     expect(electrons([1, 2, 3, 4, 5])).toBe(6);
   });
 
   it("returns 4 for [2, 2, 3, 3]", function () {
     expect(electrons([2, 2, 3, 3])).toBe(4);
-  });*/
+  });
 
    it("returns 2 for [ 6, 6, 4, 4, 1, 3 ]", function () {
     expect(electrons([6, 6, 4, 4, 1, 3])).toBe(2);
