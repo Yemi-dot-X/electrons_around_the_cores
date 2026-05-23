4f44710a872e57e43eec8ba2ff6297bf1a6c62f2
aakinrinlola <a.akinrinlola@hotmail.com>
2026-05-23 18:56:37 +0200

    Michael: test 2 refactor - es ist grün und settup ausgebessert

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -1,7 +1,7 @@
 describe("Electrons around the cores", function () {
-  /*it("returns 6 for [1, 2, 3, 4, 5]", function () {
+  it("returns 6 for [1, 2, 3, 4, 5]", function () {
     expect(electrons([1, 2, 3, 4, 5])).toBe(6);
-  });*/
+  });
 
   it("returns 4 for [2, 2, 3, 3]", function () {
     expect(electrons([2, 2, 3, 3])).toBe(4);
--- src/electrons.js
+++ src/electrons.js
@@ -1,4 +1,16 @@
 function electrons(dice) {
-  return 6;
+  let sum = 0;
+
+  for (let i = 0; i < dice.length; i++) {
+    if (dice[i] === 3) {
+      sum += 2;
+    }
+
+    if (dice[i] === 5) {
+      sum += 4;
+    }
+  }
+
+  return sum;
 }
 
