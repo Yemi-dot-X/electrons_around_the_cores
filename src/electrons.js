037fb04ab205ea4e87b24bc1f3fc8dd105e1d577
aakinrinlola <a.akinrinlola@hotmail.com>
2026-05-23 18:50:02 +0200

    Michael: test 2 funktioniert nicht

--- src/electrons.js
+++ src/electrons.js
@@ -1,15 +1,4 @@
 function electrons(dice) {
-  let sum = 0;
-
-  for (let i = 0; i < dice.length; i++) {
-    if (dice[i] === 3) {
-      sum += 2;
-    }
-
-    if (dice[i] === 5) {
-      sum += 4;
-    }
-  }
-
-  return sum;
+  return 6;
 }
+
