a99b60e9bc4a1883b45d238500e81a30946e16d9
Yemi Akinrinlola <if25b072@technikum-wien.at>
2026-05-23 16:45:45 +0200

    initial setup

---
+++ src/electrons.js
@@ -0,0 +1,15 @@
+/*function electrons(dice) {
+  let sum = 0;
+
+  for (let i = 0; i < dice.length; i++) {
+    if (dice[i] === 3) {
+      sum += 2;
+    }
+
+    if (dice[i] === 5) {
+      sum += 4;
+    }
+  }
+
+  return sum;
+}
