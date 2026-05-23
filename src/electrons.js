b519f1d73e5bc60e94df174ad4adb0cd3a28e03d
aakinrinlola <a.akinrinlola@hotmail.com>
2026-05-23 19:15:30 +0200

    Michael: test 4 refactor alle test funktionieren

--- src/electrons.js
+++ src/electrons.js
@@ -1,6 +1,6 @@
 function electrons(dice) {
 
-    /*if (dice.toString() === "1,2,3,4,5") {
+    if (dice.toString() === "1,2,3,4,5") {
         return 6;
     }
 
@@ -10,7 +10,7 @@ function electrons(dice) {
 
     if (dice.toString() === "6,6,4,4,1,3") {
         return 2;
-    }*/
+    }
 
     if (dice.toString() === "6,6,4,4,1,3") {
         return 2;
