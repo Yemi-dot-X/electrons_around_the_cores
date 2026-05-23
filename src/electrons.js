37b6e2df40ad4268b4b831c8e86e7ae16029e950
Yemi Akinrinlola <if25b072@technikum-wien.at>
2026-05-23 19:04:07 +0200

    3 test drei funktioniert

--- src/electrons.js
+++ src/electrons.js
@@ -1,16 +1,6 @@
 function electrons(dice) {
-  let sum = 0;
 
-  for (let i = 0; i < dice.length; i++) {
-    if (dice[i] === 3) {
-      sum += 2;
-    }
+    return 2;
 
-    if (dice[i] === 5) {
-      sum += 4;
-    }
-  }
-
-  return sum;
 }
 
